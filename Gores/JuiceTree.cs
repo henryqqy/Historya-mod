18d828753720f1901c3c565a165efdfc0d976191
henryqqy <67809944+henryqqy@users.noreply.github.com>
2020-09-12 18:13:52 -0300

    Add files via upload

---
+++ Gores/JuiceTree.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Terraria;
+using Terraria.ModLoader;
+using Microsoft.Xna.Framework;
+
+namespace Histoya.Gores
+{
+    public class JuiceTree : ModGore
+    {
+        public override void OnSpawn(Gore gore)
+        {   
+            gore.velocity = new Vector2(Main.rand.NextFloat() - 0.5f, Main.rand.NextFloat() * MathHelper.TwoPi);
+            gore.numFrames = 8;
+            gore.frame = (byte)Main.rand.Next(8);
+            gore.frameCounter = (byte)Main.rand.Next(8);
+            updateType = 910;
+        }
+    }
+}
